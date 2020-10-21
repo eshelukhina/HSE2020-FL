5694f3d14d0853ac6f755bc93bb8426371a948cf
Ekaterina Shelukhina <shelukchina2447@gmail.com>
2020-10-21 19:04:38 +0300

    some fixes

--- hw_06/main.py
+++ hw_06/main.py
@@ -1,12 +1,11 @@
 import sys
 
 from parsita import *
-from collections import namedtuple
 from parsita.util import constant
-from parsita.util import splat
 
 
 class PrologParser(TextParsers, whitespace='[ \t\n]*'):
+
     module = lit('module') > constant("module")
     literal = reg(r'[A-Za-z_][A-Za-z_0-9]*') > (lambda x: ["Atom", str(x)])
     identificator = reg(r'[a-z_][a-z_0-9]*') > (lambda x: ["ID", str(x)])
@@ -16,16 +15,22 @@ class PrologParser(TextParsers, whitespace='[ \t\n]*'):
     lbr = lit('(')
     rbr = lit(')')
     tstile = lit(':-') > constant("TSTILE")
+
     # ---------------------------------------------------------------
+
     mod = module & identificator & dot
+    zxl
     # ---------------------------------------------------------------
-    head = atom1 & tstile & expression & dot | atom1 & dot
+
+    head = head_atom & tstile & expression & dot | head_atom & dot
     expression = M & disunction & expression | M
     M = P & conjunction & M | P
     P = lbr & expression & rbr | atom1
     atom1 = literal & atom2 | literal
+    head_atom = identificator & atom2 | identificator
     atom2 = lbr & atom3 & rbr & atom2 | atom1 | lbr & atom3 & rbr
     atom3 = atom1 | lbr & atom3 & rbr
+
     # ----------------------------------------------------------------
 
 
@@ -44,18 +49,15 @@ def print_result(result, file_out):
 
 def main():
     in_file_name = sys.argv[1]  # input file
-    i = 0
-    dot_ix = 0
-    while i < len(in_file_name):
-        if in_file_name[i] == '.':
-            dot_ix = i
-        i += 1
-    out_file_name = in_file_name[:-(len(in_file_name) - dot_ix - 1)] + "out"  # output file
+    out_file_name = in_file_name + ".out"  # output file
     file_in = open(in_file_name)  # open file
     s = str(file_in.read())  # read file
     i = 0
     prev_pos = 0
+    mod_count = 0
     file_out = open(out_file_name, 'w')  # open output file
+    while s[len(s) - 1] == ' ' or s[len(s) - 1] == '\n' or s[len(s) - 1] == '\t':
+        s = s[:-1]
     while i < len(s):
         while i < len(s) and s[i] != '.':
             i += 1
@@ -64,8 +66,9 @@ def main():
         s = s.replace('\t', '')
         current_expression = s[prev_pos:i]
         result = str(PrologParser.mod.parse(current_expression))
-        if result[:7] == "Success":
+        if result[:7] == "Success" and mod_count < 1:
             print_result(result, file_out)
+            mod_count += 1
         else:
             result = str(PrologParser.head.parse(current_expression))
             if result[:7] == "Success":
