940179b716b0ace7a1422dd03d6629c8e03fa3dd
Ekaterina Shelukhina <shelukchina2447@gmail.com>
2020-10-21 21:19:16 +0300

    fixes

--- hw_06/main.py
+++ hw_06/main.py
@@ -5,10 +5,10 @@ from parsita.util import constant
 
 
 class PrologParser(TextParsers, whitespace='[ \t\n]*'):
-
     module = lit('module') > constant("module")
     literal = reg(r'[A-Za-z_][A-Za-z_0-9]*') > (lambda x: ["Atom", str(x)])
-    identificator = reg(r'[a-z_][a-z_0-9]*') > (lambda x: ["ID", str(x)])
+    id = reg('[a-zA-Z_][a-zA-Z_0-9]*') > (lambda x: ["ID", str(x)])
+    identificator = pred(id, lambda x: x != ['module'], 'ID')
     disunction = lit(';') > constant("DIS")
     conjunction = lit(',') > constant("CON")
     dot = lit('.') > constant("DOT")
@@ -19,7 +19,7 @@ class PrologParser(TextParsers, whitespace='[ \t\n]*'):
     # ---------------------------------------------------------------
 
     mod = module & identificator & dot
-    zxl
+
     # ---------------------------------------------------------------
 
     head = head_atom & tstile & expression & dot | head_atom & dot
@@ -47,8 +47,8 @@ def print_result(result, file_out):
     file_out.write(result[8:len(result) - 1] + '\n')
 
 
-def main():
-    in_file_name = sys.argv[1]  # input file
+def parse_non_flag_or_prog(in_file_name):
+    # print("here")
     out_file_name = in_file_name + ".out"  # output file
     file_in = open(in_file_name)  # open file
     s = str(file_in.read())  # read file
@@ -76,6 +76,74 @@ def main():
             else:
                 file_out.write("Syntax error!\n")
         prev_pos = i
+
+
+def parse_atom(in_file_name):
+    out_file_name = in_file_name + ".out"  # output file
+    file_in = open(in_file_name)  # open file
+    s = str(file_in.read())  # read file
+    i = 0
+    prev_pos = 0
+    atom_count = 0
+    file_out = open(out_file_name, 'w')  # open output file
+    while s[len(s) - 1] == ' ' or s[len(s) - 1] == '\n' or s[len(s) - 1] == '\t':
+        s = s[:-1]
+    while i < len(s) and atom_count < 1:
+        while i < len(s) and s[i] != '.':
+            i += 1
+        i += 1
+        s = s.replace('\n', '')
+        s = s.replace('\t', '')
+        current_expression = s[prev_pos:i]
+        result = str(PrologParser.head.parse(current_expression))
+        if result[:7] == "Success":
+            print_result(result, file_out)
+            atom_count += 1
+        else:
+            file_out.write("Syntax error!\n")
+            atom_count += 1
+        prev_pos = i
+
+
+def parse_module(in_file_name):
+    out_file_name = in_file_name + ".out"  # output file
+    file_in = open(in_file_name)  # open file
+    s = str(file_in.read())  # read file
+    i = 0
+    prev_pos = 0
+    mod_count = 0
+    file_out = open(out_file_name, 'w')  # open output file
+    while s[len(s) - 1] == ' ' or s[len(s) - 1] == '\n' or s[len(s) - 1] == '\t':
+        s = s[:-1]
+    while i < len(s) and mod_count < 1:
+        while i < len(s) and s[i] != '.':
+            i += 1
+        i += 1
+        s = s.replace('\n', '')
+        s = s.replace('\t', '')
+        current_expression = s[prev_pos:i]
+        result = str(PrologParser.mod.parse(current_expression))
+        if result[:7] == "Success" and mod_count < 1:
+            print_result(result, file_out)
+            mod_count += 1
+        else:
+            file_out.write("Syntax error!\n")
+            mod_count += 1
+        prev_pos = i
+
+
+def main():
+    if len(sys.argv) == 3:
+        if sys.argv[1] == "--prog":
+            parse_non_flag_or_prog(sys.argv[2])
+        elif sys.argv[1] == "--atom":
+            parse_atom(sys.argv[2])
+        elif sys.argv[1] == "--module":
+            parse_module(sys.argv[2])
+        else:
+            print("Unknown flag\n")
+    else:
+        parse_non_flag_or_prog(sys.argv[1])
 
 
 if __name__ == "__main__":
